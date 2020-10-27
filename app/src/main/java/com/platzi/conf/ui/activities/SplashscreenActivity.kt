05939f56323f44874680f6b928e50dc2a98494ac
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-27 14:23:42 -0600

    5.3 finalizando navegacion

--- app/src/main/java/com/platzi/conf/ui/activities/SplashscreenActivity.kt
+++ app/src/main/java/com/platzi/conf/ui/activities/SplashscreenActivity.kt
@@ -2,6 +2,7 @@ package com.platzi.conf.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.platzi.conf.R
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
