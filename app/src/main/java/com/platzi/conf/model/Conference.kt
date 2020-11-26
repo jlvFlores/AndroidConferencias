a4386177e455c8a7444ae9510f4378f3e22e8ad7
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-26 13:33:10 -0600

    6_1 Conectando el Proyecto con Firebase

---
+++ app/src/main/java/com/platzi/conf/model/Conference.kt
@@ -0,0 +1,11 @@
+package com.platzi.conf.model
+
+import java.util.*
+
+class Conference {
+    lateinit var title: String
+    lateinit var descripcion: String
+    lateinit var tag: String
+    lateinit var datetime: Date
+
+}
