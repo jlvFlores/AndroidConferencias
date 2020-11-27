cd5c265e88c1da4167c37f10f58a2d8e6d47c98d
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-26 19:58:19 -0600

    7_2 Creacion de adaptador para RecyclerView

--- app/src/main/java/com/platzi/conf/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/platzi/conf/view/ui/activities/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.platzi.conf
+package com.platzi.conf.view.ui.activities
 
 //import android.icu.util.Calendar
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import com.platzi.conf.R
+
 //import com.google.firebase.firestore.FirebaseFirestore
 //import com.platzi.conf.model.Conference
 //import com.platzi.conf.model.Speaker
