ce36971786996944061c724e4d01c6e532fcee83
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-27 12:26:00 -0600

    7_6 Configurando la app para ejecutar en dispositivo

--- app/src/main/java/com/platzi/conf/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/platzi/conf/view/ui/activities/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.platzi.conf.view.ui.activities
 
-//import android.icu.util.Calendar
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.annotation.RequiresApi
+import androidx.navigation.Navigation
+import androidx.navigation.NavAction
+import androidx.navigation.ui.NavigationUI
 import com.platzi.conf.R
+import com.platzi.conf.R.id.bnvMenu
 
-//import com.google.firebase.firestore.FirebaseFirestore
-//import com.platzi.conf.model.Conference
-//import com.platzi.conf.model.Speaker
-//import org.json.JSONArray
-//import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBar(findViewById(R.id.toolbarMain))
+        configNav()
 
     }
+
+    fun configNav() {
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragContent))
+    }
+
 }
