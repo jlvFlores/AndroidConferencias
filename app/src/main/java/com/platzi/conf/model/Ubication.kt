a25d8453e7dcdf8757f14ca9d1a633afdd8b091f
Jose Luis <jluisvazquez.jv@gmail.com>
2020-11-27 13:55:32 -0600

    8_2 Agregar y Personalizar un marcador al mapa

--- app/src/main/java/com/platzi/conf/model/Ubication.kt
+++ app/src/main/java/com/platzi/conf/model/Ubication.kt
@@ -1,12 +1,12 @@
 package com.platzi.conf.model
 
 class Ubication {
-    val name = ""
-    val address = ""
-    val latitude = ""
-    val longitude = ""
-    val phone = ""
-    val website = ""
-    val photo = ""
+    val name = "Centro Cultural Roberto Cantoral"
+    val address = "Puente Xoco s/n, Xoco, 03330 Ciudad de México, CDMX"
+    val latitude = 19.36202
+    val longitude = -99.166414
+    val phone = "+52 55 1519 4670"
+    val website = "https://elcantoral.com/"
+    val photo = "https://lh5.googleusercontent.com/p/AF1QipPj-rZ_uZnMzvQT3iVlA832he126T_qjFbjYiW6=w408-h270-k-no"
 
 }
--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,14 +1,22 @@
 package com.platzi.conf.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.platzi.conf.R
+import com.platzi.conf.model.Ubication
 
 /**
  * A simple [Fragment] subclass.
@@ -26,8 +34,25 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
 
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitude, ubication.longitude)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = LatLng(ubication.latitude, ubication.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("Platzi Conf 2019")
+
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150, 150, false)
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        googleMap?.addMarker(markerOptions)
     }
 
 }
